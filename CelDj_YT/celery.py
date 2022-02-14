7a28771cf1f010327d0f85442368acbaa7071215
swayam_sidd <devswayam@ingenuitas.com>
2022-02-14 21:44:41 +0530

    Added celery.py

---
+++ CelDj_YT/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+
+app = Celery('CelDj_YT')
+app.conf.enable_utc = False
+app.conf.update(timezone='Asia/Kolkata')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+# CELERY beat settings
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- CelDj_YT/settings.py
+++ CelDj_YT/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CELERY SETTINGS
+CELERY_BROKER_URL = 'redis://127.0.0.1.6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata'
