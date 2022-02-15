c1e08661bacfec3fb54a6d0256d053aeadb4212b
swayam_sidd <devswayam@ingenuitas.com>
2022-02-16 02:01:05 +0530

    All 3 done

--- CelDj_YT/celery.py
+++ CelDj_YT/celery.py
@@ -3,6 +3,8 @@ import os
 
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
+# from django_celery_beat.models import PeriodicTask
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CelDj_YT.settings')
 
@@ -15,14 +17,15 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # CELERY beat settings
 app.conf.beat_schedule = {
-    # 'send-mail-every-day-at-8': {
-    #     'task': 'send_mail_app.tasks.send_mail_func',
-    #     'schedule': crontab(hour=0, minute=46, day_of_month=19, month_of_year = 6),
-    #     #'args': (2,)
+    'send-mail-every-day-at-12': {
+        'task': 'send_mail_app.tasks.send_mail_func',
+        'schedule': crontab(hour=0, minute=21),
+        # 'schedule': crontab(hour=0, minute=46, day_of_month=19, month_of_year = 6),
+        # 'args': (2,)
     }
+}
 
 app.autodiscover_tasks()
-
 
 @app.task(bind=True)
 def debug_task(self):
